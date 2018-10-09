4e2f347da9f8cc8337a00f34f3d7c26e1764f30f
Scurvide <jani.hietala@aalto.fi>
2018-10-10 01:09:32 +0300

    Data handling done

---
+++ RaspWWW/Control/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Raspi( models.Model ):
+    name = models.CharField( max_length = 30, unique = True )
+    ip = models.GenericIPAddressField()
+
+class Datapoint ( models.Model ):
+    client = models.ForeignKey( Raspi, on_delete = models.CASCADE )
+    type = models.CharField( max_length = 30 )
+    point = models.DecimalField( max_digits = 10, decimal_places = 2 )
+    datetime = models.DateTimeField( auto_now_add = True )
