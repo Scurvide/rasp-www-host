b0f4d7d1d1f7c474fb6c3e1adbf1817a3a6dd92e
Scurvide <jani.hietala@aalto.fi>
2018-10-15 18:01:19 +0300

    Post.py created for sending POST messages

---
+++ post.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+url = 'http://192.168.1.107:8650/Control/'
+client = requests.session()
+csrf = client.get( url ).cookies[ 'csrftoken' ]
+
+payload = { 'name': 'Petteri', 'type': 'instance', 'point': 63.4311 }
+payload = json.dumps( payload )
+headers = { 'X-CSRFToken': csrf }
+
+resp = client.post( url, data = payload, headers = headers )
+print(resp.content)
